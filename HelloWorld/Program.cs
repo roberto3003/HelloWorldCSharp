fc0b50692faaad5f22c61dd8102b40ffaba0fdec
bobby3003 <roberto3003@riseup.net>
2021-09-13 09:08:25 +0700

    finished

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HelloWorld
 {
@@ -10,6 +6,26 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello, World");
+            int count = 10;
+            float totalPrice = 20.95F;
+            char character = 'A';
+            string firstName = "Roberto";
+            bool isWorking = true;
+            Console.WriteLine(isWorking);
+            Console.WriteLine(firstName);   
+            Console.WriteLine(character);
+            byte number = 2;
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            int i = 1;
+            byte b = (byte)i;
+            Console.WriteLine(i);
+            var number2 = "1234";
+            byte d = Convert.ToByte(number2);
         }
+
     }
 }
